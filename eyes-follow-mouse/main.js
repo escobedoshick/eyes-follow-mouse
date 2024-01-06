59339d059b00a181e7abd751cbb0da540b29bab6
escobedoshick <154560631+escobedoshick@users.noreply.github.com>
2024-01-06 18:38:25 +0100

    Add files via upload

---
+++ eyes-follow-mouse/main.js
@@ -0,0 +1,14 @@
+var balls = document.getElementsByClassName("ball");
+document.onmousemove = function(){
+  var x = event.clientX * 100 / window.innerWidth + "%";
+  var y = event.clientY * 100 / window.innerHeight + "%";
+  //event.clientX => get the horizontal coordinate of the mouse
+  //event.clientY => get the Vertical coordinate of the mouse
+  //window.innerWidth => get the browser width
+  //window.innerHeight => get the browser height
+  for(var i=0;i<2;i++){
+    balls[i].style.left = x;
+    balls[i].style.top = y;
+    balls[i].style.transform = "translate(-"+x+",-"+y+")";
+  }
+}
